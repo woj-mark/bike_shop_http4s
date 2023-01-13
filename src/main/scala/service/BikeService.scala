1e26197d84babdf658391718398e10e352eee271
unknown <wtm.markowski@gmail.com>
2023-01-13 18:10:03 +0000

    A route to get all the bikes works.

--- src/main/scala/service/BikeService.scala
+++ src/main/scala/service/BikeService.scala
@@ -7,59 +7,22 @@ import org.http4s._
 import org.http4s.dsl.Http4sDsl
 import cats.effect.IO
 import bikeshop.domain.bike._
-
+import io.circe.syntax._
+import org.http4s.circe._
 
 class BikeService(bikeRepo: BikeRepository) extends Http4sDsl[IO] {
       
       
-  //Decoder for the incoming JSON to a bike
-  //implicit val bikeDecoder: EntityDecoder[ F , Bike] = jsonOf[F , Bike]
+//To encode a Scala value of type A into an entity, we need an EntityEncoder[A] in scope
+//https://http4s.org/v0.16/json/
 
   val bikeRoutes=    HttpRoutes.of[IO] {
       //Get all bikes
       case GET -> Root / "bikes" =>
         val allBikes : IO[List[Bike]] = bikeRepo.findAllBikes
         allBikes.flatMap(
-            bikes => Ok(bikes)
+            bikes => Ok(bikes.asJson)
         ) 
         }
 }   
 
-
-
-   // case GET -> Root / "bikes" :? BrandQueryParamMatcher(brand) +& YearQueryParamMatcher(optionalYear) => 
-    //     val bikeByBrand = findBikesByBrand(brand)
-
-    //     optionalYear match {
-    //         case Some(yr) => 
-    //             yr.fold(
-    //             _ => BadRequest("The given year is not valid"),
-    //             {year =>
-    //                 val bikesByBrandAndYear = 
-    //                     bikeByBrand.filter(_.year == year.getValue)
-                    
-    //                 Ok(bikesByBrandAndYear.head.asJson)}
-    //             )
-    //             case None => Ok(bikeByBrand.head.asJson)
-
-
-    //     }
-
- 
-
-    
-//       //Add a new bike
-//       case req@POST -> Root / "bikes" => 
-//         for{
-//         bike <- req.as[Bike]
-//         _ = bikes.put(bike.id,bike)
-//         res <- Ok(bikes.asJson)
-//         } yield res
-//   }
-//    case GET -> Root / "bikes" =>
-//           val returnBikes: List[Bike]= bikes.values.toList
-
-//           returnBikes match {
-//             case Nil => NotFound("No bikes in the store")
-//             case _ => Ok(returnBikes.asJson)
-//           }
